d3cb1331c673cca67503fef0607f64921f4511fd
VIVEK KUMAR <106223821+vivkum066@users.noreply.github.com>
2022-11-14 23:13:54 +0530

    Update register.php

--- api/register.php
+++ api/register.php
@@ -11,7 +11,7 @@ $temp_name = $_FILES['photo']['tmp_name'];
 $role = $_POST['role'];
 
 if($password==$cpassword){
-        move_uploaded_file($tmp_name,"../uploads/$image");
+        move_uploaded_file($tmp_name,"../Uploads/$image");
         $insert = mysqli_query($connect, "INSERT INTO user (name, mobile, address, password, photo, role, status, votes) VALUES('$name','$mobile', '$address', '$password', '$image', '$role', 0,0)");
 
         if($insert){
